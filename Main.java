225d585fe2eea0d908db22431a022d1186869f26
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2021-10-27 21:53:02 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int numA[] = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("== Array A: ==");
+        for(int out:numA){
+            System.out.println(out*=out);
+        }
+
+        int numB[] = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("== Array B: ==");
+        for(int x:numB){
+            x--;
+            System.out.println(numB[x]);
+        }
+
+        int[] numC = {1,2,3,4,5,6,7,8,9,10};
+        int soma = 0;
+        System.out.println("== Array C: ==");
+        for(int x:numC){
+            soma += x;
+        } System.out.println(soma/=10);
+    }
+}
